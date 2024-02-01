99e727c96eb5dba7296b19f829db56363680a5ac
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-01-31 22:06:00 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "my-ecs-cluster"
+}
+
+resource "aws_ecs_task_definition" "my_task" {
+  family                   = "my-task-family"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name  = "my-ecs-container"
+    image = "anastasiyaohal/clockbox:latest" 
+  }])
+}
+
+resource "aws_ecs_service" "my_service" {
+  name            = "my-ecs-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets = ["subnet-0287356ba65c47876"] 
+  }
+}
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
