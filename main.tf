571982d239151b71889107cff109bdd08eb22f9f
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-01-31 22:37:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_ecs_task_definition" "my_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = aws_iam_role1.ecs_execution_role1.arn
+  execution_role_arn = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "my-ecs-container"
