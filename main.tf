aaa40d35ec02cba343f0cb87dc57ceb94797c07a
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-02-06 19:37:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,48 +10,138 @@ resource "aws_ecs_task_definition" "my_task" {
   memory                   = "512"
 
   execution_role_arn = aws_iam_role.ecs_execution_role.arn
-  task_role_arn = aws_iam_role.ecs_execution_role.arn
+ 
 
-  container_definitions = jsonencode([{
-    name  = "my-container"
-    image = "atyranazar/alpine:latest" 
-    essential = true
-      portMappings = [
+  execution_role_arn = aws_iam_role.my_api_task_execution_role.arn
+
+  container_definitions = <<EOF
+  [
+    {
+      "name": "my-api",
+      "image": "atyranazar/project:latest",
+      "portMappings": [
         {
-          containerPort = 80
-          hostPort      = 80
+          "containerPort": 3000
         }
       ]
-  }])
+    }
+  ]
+  EOF
 }
-
-resource "aws_ecs_service" "my_service" {
-  name            = "my-service"
+resource "aws_ecs_service" "my_api" {
+  name            = "my-api"
   cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task.arn
+  task_definition = aws_ecs_task_definition.my_api.arn
   launch_type     = "FARGATE"
+  
+#By default, the ECS service won’t start any containers, we need to tell it how many containers we want.
+  desired_count = 2
 
   network_configuration {
-    subnets = ["subnet-01057da043ad39ece", "subnet-097cb2e0df59d9c3c"] 
+    assign_public_ip = false
+
+    security_groups = [
+      aws_security_group.egress_all.id,
+      aws_security_group.ingress_api.id,
+    ]
+
+    subnets = [
+      aws_subnet.private_d.id,
+      aws_subnet.private_e.id,
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.my_api.arn
+    container_name   = "my-api"
+    container_port   = "3000"
+  }
+
+}
+
+# This is the role under which ECS will execute our task. 
+# The assume_role_policy field works with the following aws_iam_policy_document to allow
+# ECS tasks to assume this role we're creating.
+
+resource "aws_iam_role" "my_api_task_execution_role" {
+  name               = "my-api-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role.json
+}
+
+data "aws_iam_policy_document" "ecs_task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "ecs_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
+data "aws_iam_policy" "ecs_task_execution_role" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
-  role       = aws_iam_role.ecs_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+# This will attach the above policy to the execution role.
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+  role       = aws_iam_role.my_api_task_execution_role.name
+  policy_arn = data.aws_iam_policy.ecs_task_execution_role.arn
 }
+
+resource "aws_lb_target_group" "my_api" {
+  name        = "my-api"
+  port        = 3000
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  health_check {
+    enabled = true
+    path    = "/health"
+  }
+
+  depends_on = [aws_alb.my_api]
+}
+
+resource "aws_alb" "my_api" {
+  name               = "my-api-lb"
+  internal           = false
+  load_balancer_type = "application"
+
+  subnets = [
+    aws_subnet.public_d.id,
+    aws_subnet.public_e.id,
+  ]
+
+  security_groups = [
+    aws_security_group.http.id,
+    aws_security_group.https.id,
+    aws_security_group.egress_all.id,
+  ]
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_alb_listener" "my_api_http" {
+  load_balancer_arn = aws_alb.my_api.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_api.arn
+  }
+}
+
+output "alb_url" {
+  value = "http://${aws_alb.my_api.dns_name}"
+}
+
+# This last output block is important because it will tell us what URL 
+# we’ll use to reach the service without us having to go 
+# into the AWS console to figure it out.
+
+
+
+
