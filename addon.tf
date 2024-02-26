2ccccf79fc43fc7b5787c4091e18f356c31e6ea4
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-02-26 10:30:00 -0500

    Update addon.tf

--- addon.tf
+++ addon.tf
@@ -4,14 +4,28 @@ resource "aws_eks_addon" "vpc_cni" {
   resolve_conflicts = "OVERWRITE"
 }
 
-resource "kubernetes_config_map" "ebs_csi_driver_config" {
-  metadata {
-    name      = "ebs-csi-driver"
-    namespace = "kube-system"
-  }
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
 
-  data = {
-    "enableVolumeScheduling" = "true"
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-demo"
+  provider_url                  = replace(data.aws_eks_cluster.demo.identity.0.oidc.0.issuer, "https://", "")
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = "demo"
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.5.2-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
   }
-  depends_on = [aws_eks_cluster.demo]
 }
