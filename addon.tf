dbca5e38315a75a2ae192d5091b1492a29f828e7
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-02-26 10:53:19 -0500

    Update addon.tf

--- addon.tf
+++ addon.tf
@@ -1,9 +1,9 @@
 resource "aws_eks_addon" "cni" {
-  cluster_name = aws_eks_cluster.my_cluster.name
+  cluster_name = aws_eks_cluster.my_cluster.demo
   addon_name   = "vpc-cni"
 }
 
 resource "aws_eks_addon" "csi" {
-  cluster_name = aws_eks_cluster.my_cluster.name
+  cluster_name = aws_eks_cluster.my_cluster.demo
   addon_name   = "csi-driver"
 }
