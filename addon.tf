638db3d26d1deb23aa1ee6cdffbc86e2ebcc014a
AtyraNazar <94417099+Nazarkulova@users.noreply.github.com>
2024-02-26 13:30:02 -0500

    Update addon.tf

--- addon.tf
+++ addon.tf
@@ -1,6 +1,13 @@
+#resource "aws_eks_addon" "vpc_cni" {
+#  addon_name   = "vpc-cni"
+#  cluster_name = "demo"
+#  resolve_conflicts = "OVERWRITE"
+#}
+
 variable "addons" {
   type = list(object({
     name    = string
+    version = string
   }))
 
   default = [
@@ -10,7 +17,7 @@ variable "addons" {
   #  },
     {
       name    = "vpc-cni"
-   #   version = "v1.10.1-eksbuild.1"
+      version = "v1.16.0-eksbuild.1"
     },
    # {
    #   name    = "coredns"
@@ -18,7 +25,7 @@ variable "addons" {
    # },
     {
       name    = "aws-ebs-csi-driver"
-    #  version = "v1.4.0-eksbuild.preview"
+      version = "v1.27.0-eksbuild.1"
     }
   ]
 }
@@ -26,6 +33,7 @@ resource "aws_eks_addon" "addons" {
   for_each          = { for addon in var.addons : addon.name => addon }
   cluster_name      = aws_eks_cluster.demo.id
   addon_name        = each.value.name
-#  addon_version     = each.value.version
+  addon_version     = each.value.version
   resolve_conflicts = "OVERWRITE"
 }
+
